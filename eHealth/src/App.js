4f3ab92e6b1d448b709bf9e8ad08c4c44cab46dd
jonnycrunch <jonnycrunch@mac.com>
2016-10-01 14:10:13 -0500

    testing adding bootstrap

--- eHealth/src/App.js
+++ eHealth/src/App.js
@@ -11,7 +11,71 @@ class App extends Component {
           <h2>Welcome to eHealthWallet app</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+    <div class="jumbotron">
+      <h1>
+        Hello, @user
+      </h1>
+      <p>
+        Tell me about your medical record
+      </p>
+      <p>
+      </p>
+      <div class="form-group">
+        <label>
+          Allergies:
+        </label>
+        <select class="form-control">
+          <option value="//dynamic">
+            dynamic
+          </option>
+          <option value="salad">
+            Salad
+          </option>
+          <option value="pizzasalad">
+            Pizza and Salad
+          </option>
+          <option value="value3">
+            Item3
+          </option>
+          <option value="value4">
+            Item4
+          </option>
+        </select>
+        <div class="form-group">
+          <label>
+            Medication:
+          </label>
+          <select class="form-control">
+            <option value="pizza">
+              Pizza
+            </option>
+            <option value="salad">
+              Salad
+            </option>
+            <option value="pizzasalad">
+              Pizza and Salad
+            </option>
+          </select>
+          <div class="form-group">
+            <label>
+              Health Problems:
+            </label>
+          </div>
+        </div>
+      </div>
+      <select class="form-control">
+        <option value="pizza">
+          Pizza
+        </option>
+        <option value="salad">
+          Salad
+        </option>
+        <option value="pizzasalad">
+          Pizza and Salad
+        </option>
+      </select>
+    </div>
+
         </p>
       </div>
     );
