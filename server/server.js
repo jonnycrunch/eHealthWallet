8a8ff24f63c60a1175569334789f29249a8faa52
jonnycrunch <jonnycrunch@mac.com>
2016-10-01 21:43:42 -0500

    started getting uport persona using api

--- server/server.js
+++ server/server.js
@@ -5,6 +5,14 @@ var jsonParser = bodyParser.json();
 var fs = require('fs'); 
 var request = require('request');
 
+let MutablePersona = require('./dist/mutablePersona.js').default; 
+let Persona = require('./dist/persona.js').default; 
+let Web3 = require('web3'); 
+let ipfsApi = require('ipfs-api'); 
+let web3 = new Web3(); 
+let web3Prov = new web3.providers.HttpProvider('http://10.0.1.80:8545')
+let ipfsProv = ipfsApi('localhost', 5001)
+
 
 var fhir_file_conditions1 = '/data/home/distributed_health_hacker/distributed_health_dev/ui/server/fhir_files/conditions1.json'; 
 var fhir_file_medications1 = '/data/home/distributed_health_hacker/distributed_health_dev/ui/server/fhir_files/medications1.json'; 
@@ -194,10 +202,6 @@ app.get('/api/eastclinic/allergies', function(req, res) {
 });
 
 
-// need a resource GET endpoint for east clinic 
-// returns json in HL7 format that includes health conditions, meds, allergies, genetic testing labs results 
-
-
 // need a resource GET endpoint for north clinic 
 // returns json in HL7 format that includes health conditions, meds, allergies, genetic testing labs results 
 
@@ -207,12 +211,51 @@ app.get('/api/eastclinic/allergies', function(req, res) {
 
 
 // need a post that takes the user address, private key, and claim 
+app.post('/api/verifyclaims', jsonParser,  function(req, res) { 
+    if (!req.body) return res.sendStatus(400)
+    // expects json data as payload including the eth address, private key and the claim  
+    // use the uport persona to 
+    res.sendStatus(200);  
+}); 
+
+
+// this is saving the health care attribute to the blockchain with storage on ipfs 
+app.get('/api/makeclaim', jsonParser,  function(req, res) { 
+    if (!req.body) return res.sendStatus(400)
+    payload = req.body; 
+    var eth_add = payload.address; 
+    var claim = payload.claim;  //  this is in json format  
+    var myPrivSignKey = payload.privkey;  
+    let persona = new MutablePersona(eth_add, ipfsProv, web3Prov); 
+    persona.setPublicSigningKey(myPrivSignKey);   // need to translate the priv key to public one 
+    persona.addAttribute({ claim }, myPrivSignKey);
+    persona.writeToRegistry().then((txHash) => { 
+        console.log(txHash) 
+        res.sendStatus(200);  
+    });
+});
+
+
+app.post('/api/getclaim', jsonParser,  function(req, res) { 
+    if (!req.body) return res.sendStatus(400)
+    payload = req.body; 
+    var eth_add = payload.address; 
+    var claim_attr = payload.attr; 
+    let persona = new Persona(eth_add, ipfsProv, web3Prov);
+    persona.load().then(() => { 
+        let profile = persona.getProfile();
+        let my_claims = persona.getClaims(claim_attr);
+        let field = my_claims[0].decodedToken.payload;  
+        console.log(my_claims);   // just dump the entire payload to console. 
+        console.log(my_claims[0].decodedToken.payload); 
+        res.send(field); 
+    });
+});
 
 
 // need a get endpoint that takes a user's email and fetches the claims for an individual, returns json 
 
 
-// 
 
 
 app.listen(3001, function () {
